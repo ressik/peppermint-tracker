6872ebf533270f68df0394dc42fb1b20b3ae1f1a
ressik <ressik@gmail.com>
2025-12-21 15:06:42 -0700

    Added favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,11 +26,8 @@ export const metadata: Metadata = {
   },
   icons: {
     icon: [
-      { url: "/favicon.ico", sizes: "any" },
       { url: "/icon-96.png", sizes: "96x96", type: "image/png" },
       { url: "/icon-192.png", sizes: "192x192", type: "image/png" },
-      { url: "/icon-256.png", sizes: "256x256", type: "image/png" },
-      { url: "/icon-384.png", sizes: "384x384", type: "image/png" },
       { url: "/icon-512.png", sizes: "512x512", type: "image/png" },
     ],
     shortcut: [{ url: "/icon-192.png" }],
@@ -47,11 +44,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <link rel="icon" type="image/png" href="/favicon.png" />
-        <link rel="icon" type="image/png" sizes="96x96" href="/icon-96.png" />
-        <link rel="apple-touch-icon" href="/icon-192.png" />
-      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen`}
       >
